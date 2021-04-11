bf3ea27958ec70b7e0bdb0887545c25bebd7b5cc
luis-fernand <fernando.r_138@hotmail.com>
2021-04-11 16:51:22 -0400

    first commit

---
+++ jogo.js
@@ -0,0 +1,7 @@
+console.log('[Ferdinand] Flappu Bird')
+
+const sprites = new Image()
+sprites.src = './sprites.png'
+
+const canvas = document.querySelector('canvas')
+const contexto = canvas.getContext('2d')
